3c8058da5cb5e62aaa175510abcf28d8fb3c5883
Ahmed Babadr <ahmad@ahmadssb.com>
2015-10-03 15:56:11 +0300

    Setup controller and viewCatsList to display can names

--- L2/cat-clicker-premium/js/functions.js
+++ L2/cat-clicker-premium/js/functions.js
@@ -22,82 +22,121 @@ var catData = {
 };
 
 
+
 // view
-var HTMLDivCat = '<div id= "%divCatId%" class="divCat col-sm-12"></div>';
-var HTMLDivCatName = '<div id= "%divCatNameId%" class="col-sm-4"></div>';
-var HTMLCatImage = '<img id="%catId%" src="%imgSrc%" alt="Click Cat" class="img-responsive"/>';
-var HTMLCatName = '<h3 id="%nameId%"></h3>';
-var HTMLNumberOfClicks = '<h3 id="%NumOfClicks%"> number of clicks: <span>%counter%</span></h3>';
+//var HTMLDivCat = '<div id= "%divCatId%" class="divCat col-sm-12"></div>';
+//var HTMLDivCatName = '<div id= "%divCatNameId%" class="col-sm-4"></div>';
+//var HTMLCatImage = '<img id="%catId%" src="%imgSrc%" alt="Click Cat" class="img-responsive"/>';
+//var HTMLCatName = '<h3 id="%nameId%"></h3>';
+//var HTMLNumberOfClicks = '<h3 id="%NumOfClicks%"> number of clicks: <span>%counter%</span></h3>';
 
 var viewCats = {
 	// init
-	
+	init: function () {
+
+	},
 	// render
-	
-	 
+	render: function () {
+
+	}
 };
 
 var viewCatsList = {
-	
+	// init
+	init: function () {
+		this.HTMLDivCat = '<div id= "%divCatId%" class="divCat col-sm-12"></div>';
+		this.HTMLDivCatName = '<div id= "%divCatNameId%" class="col-sm-4"></div>';
+		this.HTMLCatImage = '<img id="%catId%" src="%imgSrc%" alt="Click Cat" class="img-responsive"/>';
+		this.HTMLCatName = '<h3 id="%nameId%"></h3>';
+		this.HTMLNumberOfClicks = '<h3 id="%NumOfClicks%"> number of clicks: <span>%counter%</span></h3>';
+		this.render();
+	},
+	// render
+	render: function () {
+		var cats = controller.getCats();
+		console.log(cats);
+		for (var i = 0; i < cats.length; i++) {
+			var divCatName =  this.HTMLDivCatName.replace('%divCatNameId%', 'divCatName' + i);
+			$('#catList').append(divCatName);
+
+			var catName = this.HTMLCatName.replace('%nameId%', 'catName' + i);
+			$('#divCatName' + i).append(catName);
+			$('#catName' + i).html(cats[i].name);
+		}
+	}
+
 };
+
+//
+//$(document).ready(function () {
+//	for (var i = 0; i < catData.cats.length; i++) {
+//		console.log("catData Length: " + catData.cats.length);
+//		var divCatName = HTMLDivCatName.replace('%divCatNameId%', 'divCatName' + i);
+//		$('#catList').append(divCatName);
+//
+//		var catName = HTMLCatName.replace('%nameId%', 'catName' + i);
+//		$('#divCatName' + i).append(catName);
+//		$('#catName' + i).html(catData.cats[i].name);
+//		(function (currentCat) {
+//			var image = catData.cats[currentCat].image;
+//			var count = catData.cats[currentCat].counter;
+//			$('#catName' + currentCat).click(function () {
+//				console.log(catData.cats[currentCat].name);
+//				var divCat = HTMLDivCat.replace('%divCatId%', 'divCat' + currentCat);
+//				$('#cats').html(divCat);
+//
+//				var imgCat = HTMLCatImage.replace('%catId%', 'catImage' + currentCat);
+//				$('#divCat' + currentCat).append(imgCat);
+//				$("#catImage" + currentCat).attr('src', catData.cats[currentCat].image);
+//
+//				var catName = HTMLCatName.replace('%nameId%', 'catName' + currentCat);
+//				$('#divCat' + currentCat).append(catName);
+//				$('#divCat' + currentCat).children('#catName' + currentCat).html(catData.cats[currentCat].name);
+//
+//				var numberOfClicksCat = HTMLNumberOfClicks.replace('%NumOfClicks%', 'counterCat' + currentCat);
+//				$('#divCat' + currentCat).append(numberOfClicksCat);
+//				$('#counterCat' + currentCat).children('span').html(catData.cats[currentCat].counter);
+//
+//				$('#catImage' + currentCat).click(function () {
+//					count++;
+//					$('#counterCat' + currentCat).children('span').html(count);
+//				});
+//
+//			});
+//		})(i);
+//
+//	}
+//});
 
 
 
 // controler
 var controller = {
 	// init
-	
+	init: function () {
+		viewCatsList.init();
+		viewCats.init();
+	},
 	// getCurrentCat
-	
+	getCurrentCat: function () {
+		return catData.currentCat;
+	},
 	// setCurrentCat(catIndex)
-	
+	setCurrentCat: function (catIndex) {
+		catData.currentCat = catIndex;
+	},
 	// getCats
-	
+	getCats: function () {
+		return catData.cats;
+	},
 	// increment counter
+	incremenCounter: function () {
+		catData.currentCat.counter++;
+		viewCats.render();
+	}
 };
 
 
-
-
-$(document).ready(function () {
-	for (var i = 0; i < catData.cats.length; i++) {
-		console.log("catData Length: " + catData.cats.length);
-		var divCatName = HTMLDivCatName.replace('%divCatNameId%', 'divCatName' + i);
-		$('#catList').append(divCatName);
-
-		var catName = HTMLCatName.replace('%nameId%', 'catName' + i);
-		$('#divCatName' + i).append(catName);
-		$('#catName' + i).html(catData.cats[i].name);
-		(function (currentCat) {
-			var image = catData.cats[currentCat].image;
-			var count = catData.cats[currentCat].counter;
-			$('#catName' + currentCat).click(function () {
-				console.log(catData.cats[currentCat].name);
-				var divCat = HTMLDivCat.replace('%divCatId%', 'divCat' + currentCat);
-				$('#cats').html(divCat);
-
-				var imgCat = HTMLCatImage.replace('%catId%', 'catImage' + currentCat);
-				$('#divCat' + currentCat).append(imgCat);
-				$("#catImage" + currentCat).attr('src', catData.cats[currentCat].image);
-
-				var catName = HTMLCatName.replace('%nameId%', 'catName' + currentCat);
-				$('#divCat' + currentCat).append(catName);
-				$('#divCat' + currentCat).children('#catName' + currentCat).html(catData.cats[currentCat].name);
-
-				var numberOfClicksCat = HTMLNumberOfClicks.replace('%NumOfClicks%', 'counterCat' + currentCat);
-				$('#divCat' + currentCat).append(numberOfClicksCat);
-				$('#counterCat' + currentCat).children('span').html(catData.cats[currentCat].counter);
-
-				$('#catImage' + currentCat).click(function () {
-					count++;
-					$('#counterCat' + currentCat).children('span').html(count);
-				});
-
-			});
-		})(i);
-
-	}
-});
 /*
 $(document).ready(function () {
 	for (var i = 0; i < catData.cats.length; i++) {
@@ -128,3 +167,4 @@ $(document).ready(function () {
 	}
 });
 */
+controller.init();
