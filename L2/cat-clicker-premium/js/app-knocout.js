5fac99329dfae564f7cc7426911e5eca27493cca
Ahmed Babadr <ahmad@ahmadssb.com>
2015-10-04 00:34:59 +0300

    Display Nicknames Using foreach binding and observableArray()

--- L2/cat-clicker-premium/js/app-knocout.js
+++ L2/cat-clicker-premium/js/app-knocout.js
@@ -2,13 +2,22 @@ var ViewModel = function () {
 	this.counter = ko.observable(0);
 	this.name = ko.observable('cat 1');
 	this.imgSrc =  ko.observable('images/cat1.jpg');
-	this.leve = ko.ob
+	
 
 	this.incremenCounter = function () {
 		this.counter(this.counter() + 1);
+		
 	};
+	
+	this.cat = ko.observableArray([
+		{nickname: 'kitty1'},
+		{nickname: 'kitty2'},
+		{nickname: 'kitty3'},
+		{nickname: 'kitty4'}
+	]);
+	
 };
-
+	
 
 
 ko.applyBindings(new ViewModel());
